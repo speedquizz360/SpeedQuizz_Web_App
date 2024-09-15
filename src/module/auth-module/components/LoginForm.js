860cbd9548f13803a870642b441b0add4d2d4fc5
saurinshah072 <saurinshah072@gmail.com>
2024-09-15 16:41:03 +0530

    change to Login
    - Update Text as Login

--- src/module/auth-module/components/LoginForm.js
+++ src/module/auth-module/components/LoginForm.js
@@ -103,7 +103,7 @@ export default function LoginForm() {
                   disabled={!values.email || !values.password}
                   // onClick={handleClick}
                 >
-                  LoginTest
+                  Login
                 </Button>
               </div>
             </Form>
